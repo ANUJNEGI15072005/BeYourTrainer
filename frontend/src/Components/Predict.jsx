5cd53413b4bff8bebf34d6dde6ee1e5fb3afa4d6
Anuj <anujn158@gmail.com>
2025-10-18 13:44:23 +0530

    Update predict.py component in frontend

--- frontend/src/Components/Predict.jsx
+++ frontend/src/Components/Predict.jsx
@@ -39,7 +39,7 @@ const Predict = () => {
 
     try {
       setLoading(true);
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://beyourtrainer.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(payload),
